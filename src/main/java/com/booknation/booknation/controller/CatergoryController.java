1b22d7d9e6681b755272464ee3393a34647d1c1d
yathu1 <yathu2198@gmail.com>
2025-06-27 12:30:58 +0530

    Adding new categories

--- src/main/java/com/booknation/booknation/controller/CatergoryController.java
+++ src/main/java/com/booknation/booknation/controller/CatergoryController.java
@@ -2,6 +2,8 @@ package com.booknation.booknation.controller;
 
 import com.booknation.booknation.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,6 +17,12 @@ public class CatergoryController {
     @GetMapping("api/public/categories")
     public  List<Category> getAllCategories() {
         return categories;
+    }
+
+    @PostMapping("api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);
+        return "Category added successfully";
     }
 
 
