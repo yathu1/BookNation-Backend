d1d22f09880f53d81fe20e38789b0d26c456171f
yathu1 <yathu2198@gmail.com>
2025-06-28 01:16:29 +0530

    Change to Response entity for all endpoints

--- src/main/java/com/booknation/booknation/controller/CategoryController.java
+++ src/main/java/com/booknation/booknation/controller/CategoryController.java
@@ -17,21 +17,22 @@ public class CategoryController {
     private  CategoryService categoryService;
 
     @GetMapping("api/public/categories")
-    public  List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public  ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @PostMapping("api/public/categories")
-    public String createCategory(@RequestBody Category category) {
+    public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
-        return "Category added successfully";
+        return new ResponseEntity<>("Category created successfully", HttpStatus.CREATED);
     }
 
     @DeleteMapping("api/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
         try {
             String status = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
+            return ResponseEntity.status(HttpStatus.OK).body(status);
         }catch (ResponseStatusException e) {
             return new ResponseEntity<>( e.getReason(),e.getStatusCode());
         }
