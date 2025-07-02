ee9e9de50f2fa7730cc97d8ad4980c20c44f9737
yathu1 <yathu2198@gmail.com>
2025-07-02 14:26:41 +0530

    added lombok and hibernate validator

--- src/main/java/com/booknation/booknation/model/Category.java
+++ src/main/java/com/booknation/booknation/model/Category.java
@@ -3,39 +3,21 @@ package com.booknation.booknation.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
 
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long categoryId;
-
-    public Category() {
-
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
+    @NotBlank
     private String categoryName;
 
 
